02295f6477ba1f5a13a3f2a562c963a1598cf19d
Programar <jp.knap@gmail.com>
2019-06-22 00:20:07 -0400

    create page and routing

--- 02 - spa/src/app/app-routing.module.ts
+++ 02 - spa/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home' , component: HomeComponent},
+  { path: 'heroes' , component: HeroesComponent},
+  { path: 'about' , component: AboutComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
